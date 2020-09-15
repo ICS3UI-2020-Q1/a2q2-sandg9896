9d5b67549231cafc88d81a79c95a228ecc6107b8
sandg9896 <sandg9896@wrdsb.ca>
2020-09-15 14:03:45 +0000

    Which number is greater?

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Gavin Sandhar
  */
 public class Main {
 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // detirmine the greater number
+
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+    
+    // ask user to enter two numbers
+    System.out.print("Please enter a number:");
+    int number1 = input.nextInt();
+
+    System.out.print("Please enter another number:");
+    int number2 = input.nextInt();
+
+    // determine which number is greater    
+    if(number1>number2){
+      // tell them if the number is greater
+      System.out.print(number1 + "is greater" );
+    }else if(number1==number2){
+      // tell them the numbers are equal
+      System.out.print(number1 + "and" + number2 + "are equal");
+    }else {
+      // or if the other number is greater
+      System.out.println(number2 + "is greater");
+    }
+
+  
     
   }
 }
